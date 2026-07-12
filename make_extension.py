9daa9934517810bfb262c29275a42259d42862bb
admin <athulkrishnasv2015@gmail.com>
2026-07-13 00:36:42 +0530

    Fix Firefox data_collection_permissions min version dependency

--- make_extension.py
+++ make_extension.py
@@ -34,10 +34,13 @@ def transform_manifest_for_firefox(manifest):
     firefox_manifest["browser_specific_settings"] = {
         "gecko": {
             "id": "anchor@athulkrishna2015",
-            "strict_min_version": "109.0"
+            "strict_min_version": "140.0",
+            "data_collection_permissions": {
+                "required": ["none"]
+            }
         },
         "gecko_android": {
-            "strict_min_version": "113.0"
+            "strict_min_version": "142.0"
         }
     }
 
